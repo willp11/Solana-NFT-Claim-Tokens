caf557669dbbdcd3a769a7cba9335dce1a54a94b
willp11 <williampage11@yahoo.co.uk>
2022-01-21 15:57:56 +0700

    added distributor account state

--- src/state.rs
+++ src/state.rs
@@ -8,11 +8,18 @@ use crate::{
     utils::try_from_slice_checked
 };
 
-pub const MAX_DISTRIBUTOR_DATA_LENGTH: usize = 1;
+// DISTRIBUTOR ACCOUNT
+pub const MAX_DISTRIBUTOR_DATA_LENGTH: usize = 1 + 32 + 32 + 32 + 32 + 8 + 8;
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct DistributorAccount {
-    pub is_initialized: bool
+    pub is_initialized: bool,
+    pub authority: Pubkey, 
+    pub nft_mint: Pubkey,
+    pub reward_mint: Pubkey,
+    pub reward_pool: Pubkey,
+    pub reward_amount: u64,
+    pub start_ts: i64
 }
 
 impl DistributorAccount {
